5dc6e938d9827f75ec4dd4a415451e693e1628e8
aslı saadet ergin <erginsaadet03@gmail.com>
2021-05-23 02:17:37 +0300

    10.Gün 4.Ödev

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -8,8 +8,7 @@ namespace Core.DataAccess
 {
    public interface IEntityRepository<T> where T:class,IEntity, new()
     {
-        List<T> GetById(int customerId);
-
+     
         //List<T> GetAll();
         List<T> GetAll(Expression<Func<T,bool>> filter=null); //filtrelemede 
         T Get(Expression<Func<T, bool>> filter);  //bir şeyin detayını getirirken
